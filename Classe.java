e5fce62bb14d35b7b2d19ccde061dbca6ad55550
carvalhocamila <101676220+carvalhocamila@users.noreply.github.com>
2023-03-16 00:28:09 -0400

    Create Classe.java

---
+++ Classe.java
@@ -0,0 +1,11 @@
+public class Classe {
+    
+    public static void imprimirMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+           System.out.println(a+b);
+    }
+}
